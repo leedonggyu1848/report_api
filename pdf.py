39ef50b8c1407504f39330b6f5d93ec2b65a3004
leedonggyu1848 <leedong1848@gmail.com>
2022-04-18 15:05:59 +0900

    add error handling

--- pdf.py
+++ pdf.py
@@ -2,7 +2,7 @@ from PyPDF2 import PdfFileReader
 import io
 import requests
 
-def download_pdf(url, path, min_pages_num=10):
+def download_pdf(url, path, min_pages_num=25):
   headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36'}
   res = requests.get(url, headers=headers)
   reader = PdfFileReader(io.BytesIO(res.content))
@@ -13,4 +13,7 @@ def download_pdf(url, path, min_pages_num=10):
 def download_pdf_with_list(items, path):
   for i, item in enumerate(items):
     print(i + ' / ', len(items))
-    download_pdf(item['pdf_link'], path + item['pdf_name'])
+    try:
+      download_pdf(item['pdf_link'], path + item['pdf_name'])
+    except:
+      pass
